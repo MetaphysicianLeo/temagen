88bc507670fdf01a6152132fcf82bc609bcd5270
MetaphysicianLeo <1186491507@qq.com>
2020-11-23 00:27:33 +0800

    restrcuting

--- main.py
+++
@@ -1,5 +0,0 @@
-#! /usr/env python3
-
-import PIL as p
-import numpy as np
-import sys
--- temagen/main.py
+++ temagen/main.py
@@ -6,41 +6,24 @@ import argparse
 import sys
 from enum import Enum
 
+
 class Algorithm:
-    def __init__(self, type):
-        self.selector = {
-            AlgorithmType.k_means: self._k_means
-        }
-        self.select(type)
-        self.run()
 
-    def run(self):
-        self.selector[self.getType()]()
-
-    def _k_means(self):
-        print("success")
-        
-    def getType(self):
-        return self._type
-
-
-    def select(self, type):
-        try:
-            self._type = AlgorithmType(type)
-        except ValueError as e:
-            raise
+    def __init__(self, algorithm_type: AlgorithmType):
+        pass
 
 
 class AlgorithmType(Enum):
     k_means = "k_means"
 
+
 def set_args():
     parser = argparse.ArgumentParser(prog="temagen")
     parser.add_argument('image', help="image_path", type=str)
     parser.add_argument('-n', help="the number of the theme", dest="number", type=int, default=16)
-    parser.add_argument('-p', '--picture', help="output a image", dest="image", type=str)
-    parser.add_argument('-l', '--less', help="wild color mode", dest="number", action="store_const", const=8)
-    parser.add_argument('-a', '--algorithm', help="")
+    parser.add_argument('-p', '--picture', help="output a image", dest="image", type=argparse.FileType("w"))
+    parser.add_argument('-s', '--short', help="short color mode", dest="number", action="store_const", const=8)
+    parser.add_argument('-a', '--algorithm', help="", type=AlgorithmType)
     return parser.parse_args()
 
 
